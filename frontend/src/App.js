39217b107895d1274d757af92c73c8ab766e6b8e
Henrique Panham <henriquepjv@hotmail.com>
2020-04-12 14:58:16 -0300

    Creates front initial structure

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.scss";
+import { Container, Section } from 'rbx';
+
+const App = () => (
+  <div>
+    <Section>
+      <Container>
+        IHungry
+      </Container>
+    </Section>
+  </div>
+)
+
+export default App;
